440b459c48fcf570486a33a178c65ee74e0d0a39
Olzhas Suleimen <amajisenpai@gmail.com>
2024-12-03 17:22:48 +0500

    Init.

---
+++ bin/picohttpparser.dart
@@ -0,0 +1,5 @@
+import 'package:picohttpparser/picohttpparser.dart';
+
+void main() {
+  print('Hello world: ${calculate()}!');
+}
---
+++ test/picohttpparser_test.dart
@@ -0,0 +1,8 @@
+import 'package:picohttpparser/picohttpparser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
